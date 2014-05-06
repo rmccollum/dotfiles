b4e4fc9689e56291bfa843aa692d347494ec9560
ryan <ryanmccollum@Ryans-MacBook-Pro.local>
2014-05-06 07:57:01 +0100

    Adding install file

---
+++ install.sh
@@ -0,0 +1,15 @@
+declare -a arr=(".aliases" ".bash_prompt")
+dest_dir=~
+cwd=$(pwd)
+
+echo $cwd
+
+pushd $dest_dir > /dev/null 2>&1
+
+for i in "${arr[@]}"
+do
+   echo "linking $dest_dir/$i to $cwd/$i"
+   ln -sf $cwd/$i $i
+done
+
+popd > /dev/null 2>&1
